3695136bea3fd799a49f1d710bf86fef16491574
Florian Beaumont <25250981+BFlorian91@users.noreply.github.com>
2024-07-31 00:45:44 +0200

    feat: add homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { ArticlesComponent } from './components/articles/articles.component';
 import { ListOfCatsComponent } from './components/list-of-cats/list-of-cats.component';
 import { ListOfUsersComponent } from './components/list-of-users/list-of-users.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'cats', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
   { path: 'cats', component: ListOfCatsComponent },
   { path: 'users', component: ListOfUsersComponent },
   { path: 'articles', component: ArticlesComponent },
