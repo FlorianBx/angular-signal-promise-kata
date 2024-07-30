b79fbbe517d30aaebf8219c7cefd4f2718832c14
Florian Beaumont <25250981+BFlorian91@users.noreply.github.com>
2024-07-30 21:32:09 +0200

    feat: update router with the new route /users and init service for users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ArticlesComponent } from './components/articles/articles.component';
 import { ListOfCatsComponent } from './components/list-of-cats/list-of-cats.component';
+import { ListOfUsersComponent } from './components/list-of-users/list-of-users.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'cats', pathMatch: 'full' },
   { path: 'cats', component: ListOfCatsComponent },
+  { path: 'users', component: ListOfUsersComponent },
   { path: 'articles', component: ArticlesComponent },
 ];
