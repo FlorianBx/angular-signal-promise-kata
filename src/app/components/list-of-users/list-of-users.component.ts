d89e5555deb543b08b97cbadce60b0ace9fe6e59
Florian Beaumont <25250981+BFlorian91@users.noreply.github.com>
2024-07-31 00:34:11 +0200

    feat: filter in the users page, and some minor css fix on other component

--- src/app/components/list-of-users/list-of-users.component.ts
+++ src/app/components/list-of-users/list-of-users.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { FormsModule } from '@angular/forms';
+import { User } from '../../Models/user';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-list-of-users',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './list-of-users.component.html',
-  styleUrl: './list-of-users.component.css'
+  styleUrl: './list-of-users.component.css',
 })
 export class ListOfUsersComponent {
+  listOfUsers = signal<User[]>([]);
+  filterLetter = signal('');
+  isLoading = signal(true);
 
+  constructor(private usersService: UsersService) {}
+
+  async ngOnInit() {
+    try {
+      const users: User[] = await lastValueFrom(this.usersService.getUsers());
+      if (!users) throw new Error('No users found');
+      this.listOfUsers.set(await this.filterUsersAsync(users));
+    } catch (error) {
+      console.error('Error fetching or processing users:', error);
+    } finally {
+      this.isLoading.set(false);
+    }
+  }
+
+  async filterUsersAsync(users: User[]): Promise<User[]> {
+    return new Promise((resolve, reject) => {
+      try {
+        const filteredUsers = users.filter((user: User) =>
+          user.username.includes(this.filterLetter()),
+        );
+        resolve(filteredUsers);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  async onFilterChange(event: KeyboardEvent) {
+    const inputElement = event.target as HTMLInputElement;
+    this.filterLetter.set(inputElement.value);
+    if (!inputElement.value) {
+      this.filterLetter.set('');
+      this.listOfUsers.set(await lastValueFrom(this.usersService.getUsers()));
+    }
+    this.listOfUsers.set(await this.filterUsersAsync(this.listOfUsers()));
+  }
 }
